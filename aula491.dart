8cec367eb5cc1dd7d831b2e167587b10f97e16ca
messiasps <messiaspereira407@gmail.com>
2022-08-24 12:31:34 -0300

    fundamentos

---
+++ aula491.dart
@@ -0,0 +1,9 @@
+void main() {
+  
+  //Valor constante, não é possivel add elementos na lista
+  final lista = const ['Assis', 'Marleide', 'Márcio'];
+  lista.add('Messias');
+
+  print(lista);
+
+}
