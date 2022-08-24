780512f3071f152c7143dff30d333ef0dbb8fc71
messiasps <messiaspereira407@gmail.com>
2022-08-24 10:23:28 -0300

    fundametos

--- aula487.dart
+++ aula487.dart
@@ -1,1 +1,23 @@
-//Operadores 01
+void main() {
+  
+  //Operadores binarios. (a + b) é notação é conhecida como INFIXA, 
+  //é uma notação comum em aritmética e na lógica, onde os operadores 
+  //são colocados entre os operandos em que eles atuam.
+  int a = 7;
+  int b = 3;
+
+  print(a + b);
+  print(a / b);
+  print(a * b);
+  print(a % b);
+
+  //Operadores lógicos
+  bool ehFragil = true;
+  bool ehCaro = true;
+
+  print(ehFragil && ehCaro);
+  print(ehFragil || ehCaro);
+  print(ehFragil ^ ehCaro);//OU Exclusivo, é obrigatorio um dos dois ser true.
+  print(!ehFragil);//Operador unário e notação pré-fixada (Prefix), operador vem antes do operando.
+  
+}
