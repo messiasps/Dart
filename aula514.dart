0226a18a9cfdbcfb1251c63d1e2db24c41ddae4b
messiasps <messiaspereira407@gmail.com>
2022-08-26 12:07:12 -0300

    funções

--- aula514.dart
+++ aula514.dart
@@ -1,3 +1,9 @@
 main() {
-  
+  var adicao = (int a, int b) {
+    return a + b;
+  };
+  print(adicao(3,4));
+
+  var subtracao = (int a, int b) => a - b;//Faz a mesma coisa do código acima, permitem ter um retorno implícito
+  print(subtracao(4,3));
 }
