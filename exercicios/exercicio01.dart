1cdf158eb02f0697e5b6f81058f8f71c616a5723
messiasps <messiaspereira407@gmail.com>
2022-08-29 09:40:33 -0300

    exercicio

--- exercicios/exercicio01.dart
+++ exercicios/exercicio01.dart
@@ -1,3 +1,11 @@
+//17 - Escreva um programa que leia um determinado valor positivo e calcule o seu dobro.
+import 'dart:io';
 void main() {
   
+  stdout.write('Digite um valor positivo ');
+  var valor = stdin.readLineSync()!;
+
+  var resultado = double.parse(valor);
+  print('O dobro do valor $valor é ${resultado * 2}');
+
 }
