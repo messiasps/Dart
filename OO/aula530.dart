0940c2f99e801a2c6a3f125f2e25f28a06d50918
messiasps <messiaspereira407@gmail.com>
2022-08-30 13:24:13 -0300

    oo

---
+++ OO/aula530.dart
@@ -0,0 +1,21 @@
+//Parametros nomeados
+class Data {
+  //lat: permite que o valor inicialmente seja nulo
+  int dia;
+  int mes;
+  int ano;
+
+  //Name Constructors
+  Data.com({this.dia = 1, this.mes = 1, this.ano = 1970});//parametro nomeados
+
+  String obter() {
+    return '$dia/$mes/$ano';
+  }
+}
+main() {
+
+  var dataFinal = Data.com(dia: 23, mes: 7, ano: 2024);
+
+  String d1 = dataFinal.obter(); 
+  print(d1);
+}
